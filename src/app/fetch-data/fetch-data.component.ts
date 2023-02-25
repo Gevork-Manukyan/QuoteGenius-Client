3c9494593d02c95fac94bb96330cf11209c54a0a
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-02-24 18:15:05 -0800

    call quote endpoint and display quotes

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent {
+  public quotes: Quote[] = []
+  baseUrl = "https://localhost:7064/"
 
+  constructor(http: HttpClient) {
+    http.get<Quote[]>(this.baseUrl + 'quote').subscribe(result => {
+      this.quotes = result
+    }, error => console.error(error));
+  }
+}
+
+interface Quote {
+  date: string,
+  text: string,
+  author: string
 }
